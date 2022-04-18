75356593269ff52b6d9f34ef8ba2a119df6a290d
janenjihia <jane.njihia@student.moringaschool.com>
2022-04-18 15:18:36 +0300

    modify date pipe

--- src/app/date-pipe.pipe.ts
+++ src/app/date-pipe.pipe.ts
@@ -5,8 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipePipe implements PipeTransform {
   transform(value: any, ...args: any[]): any{
-  
     return null;
-  
 }
 }
